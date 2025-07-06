342119f8a45a1362e0fb2a0cd967f9e142cae4d6
Nikita Bakutov <nikitabakutov2008@gmail.com>
2025-07-07 00:35:36 +0300

    в utils добавлена функция для получения активного центра белка

--- fpgen/utils.py
+++ fpgen/utils.py
@@ -12,6 +12,7 @@ import os
 from pdbfixer import PDBFixer
 from openmm.app import PDBFile
 from openmm import Platform
+import MDAnalysis as mda
 
 from esm.sdk.api import ESMProtein
 
@@ -23,7 +24,7 @@ def fix_protein(
     ) -> ESMProtein:
 
     '''
-    Исправляет белок
+    Исправляет белок, добавляет полноатомную структуру
     '''
 
     root_dir = os.getcwd()
@@ -44,3 +45,32 @@ def fix_protein(
     fixed_protein = ESMProtein.from_pdb(output_path)
 
     return fixed_protein
+
+def get_active_site_residues(
+        target_residues: tuple,
+        protein: ESMProtein | None = None,
+        input_filename: str | None = None,
+        output_filename: str = 'temp_protein.pdb',
+        radius: float = 5.0
+    ) -> list[int]:
+    
+    '''
+    Возвращает индексы остатков в радиусе 'radius' Å от 'target_residues'.
+    '''
+
+    if len(target_residues) != 2:
+        ValueError('target_residues должен содержать два числа: индекс начала и конца')
+
+    if not ((protein is not None) ^ (input_filename is not None)):
+        ValueError('Необходимо передавать либо белок ESMProtein, либо путь к файлу .pdb')
+
+    # Чтение белка
+    if protein is not None:
+        protein.to_pdb(output_filename)
+        input_filename = output_filename
+
+    uni = mda.Universe(input_filename)
+    selection = uni.select_atoms(f'around {radius} resid {target_residues[0]}-{target_residues[1]}')
+    residues = list(set(selection.residues.resids - 1)) + list(range(target_residues[0] - 1, target_residues[1]))
+    
+    return list(sorted(residues))
